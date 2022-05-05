2aa641dc97492c1e1d20d5f106fd089da6f39df7
Rafael Z Tanganelli <kabelu@gmail.com>
2022-05-04 20:41:11 -0500

    Adding DocString, external library and some variables

--- hello.py
+++ hello.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
 
-'''
-Print Hello world 
-'''
+#DocString
+""" Hello World Multi Language
 
-print('Hello, world!'.upper())
+Print Hello world in different languages depends on env var LANG
 
+How to use:
+You should have env var LANG setted
+
+    export LANG=pt_BR
+
+Execution:
+    python3 hello.py
+    or
+    ./hello.py
+"""
+# Dunder
+__version__ = "0.0.1"
+__author__ = "Rafael Tanganelli"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, world!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg.upper()) # built-in
+    
