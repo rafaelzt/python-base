e9dc8de98f623c8fd35fe32b99b000a891f93c0c
Rafael Z Tanganelli <kabelu@gmail.com>
2022-05-09 19:22:08 -0500

    Using dictionaries

--- escola_v3_dict.py
+++ escola_v3_dict.py
@@ -4,20 +4,20 @@
 
 Print a list of students grouped by class and activities.
 """
-__version__ = "0.1.0"
+__version__ = "0.1.22"
 
 # Dados
 sala1 = ["Erik","Maia","Gustavo","Manuel","Sofia","Joana"]
 sala2 = ["Joao","Antonio","Carlos","Maria","Isolda"]
 
-aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
-aula_musica = ["Erik", "Carlos", "Maria"]
-aula_danca = ["Gustavo","Sofia","Joana","Antonio"]
+atividades = {
+    "ingles": ["Erik","Maia","Joana","Carlos","Antonio"],
+    "musica": ["Erik","Carlos","Maria"],
+    "danca": ["Gustavo","Sofia","Joana","Antonio"]
+}
 
-atividades = [("Inglês", aula_ingles), ("Música", aula_musica), ("Dança", aula_danca)]
 # Listar alunos em cada atividade por sala
-
-for nome_atividade, atividade in atividades:
+for nome_atividade, atividade in atividades.items():
     print(nome_atividade)
     print(f"Sala 1 -> {set(sala1).intersection(atividade)}")
     print(f"Sala 2 -> {set(sala2).intersection(atividade)}")
