a23e5de66e4b8d5a840aee0946650dfd6084852e
Rafael Z Tanganelli <kabelu@gmail.com>
2022-05-12 14:09:41 -0500

    Using try except in errors.py

--- errors.py
+++ errors.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
-import os
+
 import sys
 
-# LBYL - Look Before You Leap
-if os.path.exists("names.txt"):
-    print("O arquivo existe!")
-    input("...") # RACE Condition
-    names = open("names.txt").readlines()
-else:
+# EAFP - Easy to Ask Forgiveness than permission
+# É mais fácil pedir perdão do que permissão)
+
+try:
+    names = open("names.txt").readlines() # FileNotFoundError
+    1 / 0 # ZeroDivisionError
+    print(names.banana) # AttributeError
+except FileNotFoundError:
     print(f"[Err] File names.txt not found.")
     sys.exit(1)
+except ZeroDivisionError:
+    print(f"[Err] You can not divide by zero")
+    sys.exit(2)
+except AttributeError:
+    print(f"Attribute does not exists!")
+    sys.exit(3)
 
-if len(names) >= 3:
-    print(names[2])
-else:
+try:
+    print(names[3])
+except: # Bare except
     print("[Error] Missing name in the list")
-    sys.exit(1)
+    sys.exit(4)
