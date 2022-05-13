2eef6aff4d63274b305d97a2582f81edf6e28dc8
Rafael Z Tanganelli <kabelu@gmail.com>
2022-05-12 22:36:36 -0500

    Adding PermissionError on infixcalc_v3.py

--- infixcalc_v3.py
+++ infixcalc_v3.py
@@ -77,7 +77,14 @@ filepath = os.path.join(path, "infixcalc.log")
 timestamp = datetime.now().isoformat()
 user = os.getenv("USER", "anonymous")
 
-with open(filepath, "a") as file_:
-    file_.write(f"{timestamp} / {user} -> {operation},{n1},{n2} = {result}\n")
+
 
 print(f"O resultado da operação {operation} é {result}")
+
+try:
+    with open(filepath, "a") as file_:
+        file_.write(f"{timestamp} / {user} -> {operation},{n1},{n2} = {result}\n")
+except PermissionError as e:
+    print(f"{str(e)}")
+    sys.exit(1)
+
